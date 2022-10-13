b277f4eb604af0f659d968c9d5b075a702ad9f0e
João Marcelo Daltro Marques <90789618+celinhodaltro@users.noreply.github.com>
2022-10-13 16:41:40 -0300

    Adicionar arquivos de projeto.

---
+++ Cadastro/ViewModels/ProductViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadastro.ViewModels
+{
+    public class ProductViewModel
+    {
+        [Key]
+        [Display(Name = "Código")]
+        [Required(ErrorMessage = "O código é requerido.")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O nome é requerido.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Sobrenome")]
+        [Required(ErrorMessage = "O sobrenome é requerido.")]
+        public double Value { get; set; }
+
+        [Required(ErrorMessage = "O email é requerido.")]
+        public int AvailableQuantity { get; set; }
+
+        [Required(ErrorMessage = "Um cliente é necessario.")]
+        public List<ClientViewModel> ClientList { get; set; }
+    }
+}
